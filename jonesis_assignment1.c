0ebe96a12d42112d6c428afa59cfd1b0a544b862
Isabella Jones <jonesis@oregonstate.edu>
2025-07-04 04:28:42 +0000

    Added beginning questions for program.

--- jonesis_assignment1.c
+++ jonesis_assignment1.c
@@ -2,5 +2,21 @@
 #include <math.h>
 
 int main(void) {
+    // Declaring Variables
+    float segment;
+
+    // Evaluation Questions
+    printf("How many spherical segments do you want to evaluate [2-10]?\n");
+    scanf("%f", &segment);
+    
+    // Check for correct value
+    if (segment < 2) {
+        return 0;
+    } else if (segment > 10) {
+        return 0;
+    } else {
+        printf("Obtaining data for spherical segment number 1\n");
+    }
+
     return 0;
 }
